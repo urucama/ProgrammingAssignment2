b64393598486f195f7ec64fd7d0b57d0006770a0
urucama <cedric.zioueche@gmail.com>
2014-11-23 18:11:39 +0100

    final version before sumission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions solving the inversion of a matrix, reducing processing time using cached results.
 
-## Write a short comment describing this function
+## Can be tested using the makeCacheMatrix with a invertible matrix like this one: matrix(c(1,0,5,2,1,6,3,4,0),3,3)
+## the list returned must be stored in a value that will used as an argument for the cacheSolve function.
+## The cachesolve function will then display the inverted matrix and specify if the result is cached.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## MakeCacheMatrix - takes a matrix x as argument // returns a list of getters and setters functions that allows to get or set the current
+## cached matrix x and the inverted one invmat.
+
+makeCacheMatrix <- function(x) {
+    
+    invmat <- NULL
+    
+    set <- function(y){
+        x <<- y
+        invmat <<- NULL
+    }
+    
+    get <- function() x
+    
+    set_im <- function(inverted_matrix){
+        invmat <<- inverted_matrix
+    }
+    
+    get_im <- function() invmat
+    
+    list(get = get, set = set, set_im = set_im, get_im = get_im)
 }
 
 
-## Write a short comment describing this function
+## Cachsolve takes a matrix x as argument // Returns the inverse of a matrix whenever its exists one. It details wether
+## the result is cached or not. If not cached, it calculates it and store it in the object created by the MakeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    invmat <- x$get_im()
+    
+    if (!is.null(invmat)){
+        
+        print("Getting cached result")
+        return (invmat)
+    }
+    
+    mat <- x$get()
+    invmat <- solve(mat) 
+    x$set_im(invmat)
+    invmat
 }
